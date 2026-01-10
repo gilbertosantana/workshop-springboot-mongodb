977b67d952cc121477347f9c9567d9b19b76df6d
gilbertosantana <gilbertosantoss307@gmail.com>
2026-01-10 10:17:46 -0300

    Obtendo um usuário por id

--- src/main/java/com/gilbertosantana/workshopmongo/services/UserService.java
+++ src/main/java/com/gilbertosantana/workshopmongo/services/UserService.java
@@ -1,11 +1,13 @@
 package com.gilbertosantana.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.gilbertosantana.workshopmongo.domain.User;
 import com.gilbertosantana.workshopmongo.repository.UserRepository;
+import com.gilbertosantana.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,5 +20,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
