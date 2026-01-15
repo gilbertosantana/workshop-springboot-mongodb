735279c815a6c0d2416af0a116cc2858a3acaa3d
gilbertosantana <gilbertosantoss307@gmail.com>
2026-01-15 16:39:36 -0300

    Consulta com varios criterios

--- src/main/java/com/gilbertosantana/workshopmongo/services/PostService.java
+++ src/main/java/com/gilbertosantana/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.gilbertosantana.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,7 +26,11 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text) {
 		return repository.searchTitle(text);
-		//return repository.findByTitleIgnoreCase(text);
-		
+		//return repository.findByTitleIgnoreCase(text);	
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repository.fullSearch(text, minDate, maxDate);
 	}
 }
